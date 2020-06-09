c342cb3eab57beea180b37d2fb5e78387d4df327
Parikshit Gupta <45290746+parikshitgupta1@users.noreply.github.com>
2020-06-09 13:02:18 +0530

    day 9

---
+++ June leetcoding challenge/Is Subsequence.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        for char in s:
+            if char in t:
+                t = t[t.index(char)+1:]
+            else:
+                return False
+        return True
