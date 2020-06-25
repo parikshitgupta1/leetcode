5d1ccf8f5cee1a0e646ac6c0792bbd33375ee324
Parikshit Gupta <45290746+parikshitgupta1@users.noreply.github.com>
2020-06-25 14:27:42 +0530

    day 25

---
+++ June leetcoding challenge/Find the Duplicate Number.py
@@ -0,0 +1,7 @@
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        for i in range(0,len(nums)):
+            if(nums[abs(nums[i])]<0):
+                return abs(nums[i])
+            else:
+                nums[abs(nums[i])] = -nums[abs(nums[i])]
