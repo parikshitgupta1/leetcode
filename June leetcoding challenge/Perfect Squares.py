36d8a92d971d8ba61d5c32cfc872cf2638a82272
Parikshit Gupta <45290746+parikshitgupta1@users.noreply.github.com>
2020-06-28 15:34:08 +0530

    new executed Runtime Error

--- June leetcoding challenge/Perfect Squares.py
+++ June leetcoding challenge/Perfect Squares.py
@@ -1,17 +1,16 @@
-import sys
 class Solution(object):
     def numSquares(self, n):
-        """
-        :type n: int
-        :rtype: int
-        """
-        dp = [sys.maxsize for i in range(0,n+1)]
-        dp[0] = 0
-        dp[1] = 1
-        for i in range(2,n+1):
-            j = 1
-            while(j*j<=n):
-                dp[i] = min(dp[i],1+dp[i-(j*j)])
-                j+=1
-        
+        from math import ceil, sqrt
+        dp = [0, 1, 2, 3]
+        if n<=3:
+            return dp[n]
+        else:
+            for i in range(4, n + 1):
+                dp.append(i)
+                for x in range(1, int(ceil(sqrt(i))) + 1):
+                    temp = x*x
+                    if temp>i:
+                        break
+                    else:
+                        dp[i] = min(dp[i], 1 + dp[i - temp])
         return dp[n]
