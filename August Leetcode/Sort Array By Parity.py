fc20b3f33328d05a7135491fbbf3f63ee9e512ab
Parikshit Gupta <45290746+parikshitgupta1@users.noreply.github.com>
2020-08-21 21:05:41 +0530

    day 21

---
+++ August Leetcode/Sort Array By Parity.py
@@ -0,0 +1,10 @@
+class Solution:
+    def sortArrayByParity(self, A: List[int]) -> List[int]:
+        j=0
+        for i in range(len(A)):
+            if (A[i]%2)==0:
+                temp = A[i]
+                A[i]=A[j]
+                A[j]=temp
+                j+=1
+        return A
