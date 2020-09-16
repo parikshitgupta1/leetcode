8df326c423361675939a80c0639553dedddf6acc
Parikshit Gupta <45290746+parikshitgupta1@users.noreply.github.com>
2020-09-16 23:32:08 +0530

    day 15

---
+++ September/Length of Last Word.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def lengthOfLastWord(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        l = s.split()
+        if len(l):
+            return len(l[-1])
+        else:
+            return 0
