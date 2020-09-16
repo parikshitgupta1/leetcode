9605dce1e0128238c4bd33446e46c9f07512fff6
Parikshit Gupta <45290746+parikshitgupta1@users.noreply.github.com>
2020-09-16 23:32:46 +0530

    day 16

---
+++ September/Maximum XOR of Two Numbers in an Array.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def findMaximumXOR(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        largest = 0
+        l = len(bin(max(nums))[2:])
+        for i in range(l)[::-1]:
+            largest = largest << 1
+            pre = {n >> i for n in nums}
+            largest = largest + any(largest^1 ^p in pre for p in pre)
+        return largest
