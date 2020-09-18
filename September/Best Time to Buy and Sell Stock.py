5e9e79cc54fa4e6900226a86fb5b85e79536efe9
Parikshit Gupta <45290746+parikshitgupta1@users.noreply.github.com>
2020-09-18 21:17:15 +0530

    day 18

---
+++ September/Best Time to Buy and Sell Stock.py
@@ -0,0 +1,8 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        buy = float(inf)
+        sell = 0
+        for price in prices:
+            buy = min(buy, price)
+            sell = max(sell, price - buy)
+        return sell
