86a20d30252d7e91c2b5875f812d190ba04b4319
Parikshit Gupta <45290746+parikshitgupta1@users.noreply.github.com>
2020-10-08 22:25:09 +0530

    day 8

---
+++ October/Binary Search.py
@@ -0,0 +1,7 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        idx = bisect_left(nums,target)
+        
+        if idx != len(nums) and nums[idx] == target:
+            return idx 
+        return -1
