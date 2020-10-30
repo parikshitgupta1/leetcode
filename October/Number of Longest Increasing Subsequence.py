0d0dbb28f23f8f3e363675479f0a749e8dfacb36
Parikshit Gupta <45290746+parikshitgupta1@users.noreply.github.com>
2020-10-30 21:58:43 +0530

    day 30

---
+++ October/Number of Longest Increasing Subsequence.py
@@ -0,0 +1,13 @@
+class Solution:
+    def findNumberOfLIS(self, nums):
+        dp = collections.defaultdict(collections.Counter)
+        dp[-1][-1e9] = 1
+        table = []
+        for i in nums:
+            index = bisect.bisect_left(table, i)
+            if index == len(table):
+                table.append(i)
+            else:
+                table[index] = i 
+            dp[index][i] += sum(dp[index-1][j] for j in dp[index-1] if j < i)
+        return sum(dp[max(0, len(table)-1)].values()) 
