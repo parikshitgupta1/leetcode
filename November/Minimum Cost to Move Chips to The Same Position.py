c4f684055a9b726768ab8cd921d51b1f125702eb
Parikshit Gupta <45290746+parikshitgupta1@users.noreply.github.com>
2020-11-05 21:19:56 +0530

    day 5

---
+++ November/Minimum Cost to Move Chips to The Same Position.py
@@ -0,0 +1,9 @@
+class Solution:
+	def minCostToMoveChips(self, positions: List[int]) -> int:
+		even, odd = 0, 0
+		for position in positions:
+			if position % 2 == 0:
+				even += 1
+			else:
+				odd += 1
+		return min(even, odd)
