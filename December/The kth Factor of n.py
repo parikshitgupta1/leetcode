5549f6260698b44b28ff6272e941159f143d9c9b
Parikshit Gupta <45290746+parikshitgupta1@users.noreply.github.com>
2020-12-04 21:21:27 +0530

    day 4

---
+++ December/The kth Factor of n.py
@@ -0,0 +1,7 @@
+class Solution:
+    def kthFactor(self, n: int, k: int) -> int:
+        factors = [i for i in range(1, n + 1) if n % i == 0]
+        print(factors)
+        if k <= len(factors):
+            return factors[k - 1]
+        return -1
