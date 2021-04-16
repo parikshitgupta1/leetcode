4b0485cacdc20b74f78fbd5f2a73e2d763011091
Parikshit Gupta <45290746+parikshitgupta1@users.noreply.github.com>
2021-04-16 21:13:29 +0530

    day 16

---
+++ April 2021/Remove All Adjacent Duplicates in String II.py
@@ -0,0 +1,13 @@
+class Solution:
+    def removeDuplicates(self, s: str, k: int) -> str:
+        # This is a dummy value to avoid index error or repeated len checks
+        stack = [['#', 0]]
+
+        for c in s:
+            if stack[-1][0] == c:
+                stack[-1][1] += 1
+                if stack[-1][1] == k:
+                    stack.pop()
+            else:
+                stack.append([c, 1])
+        return "".join([c*i for c, i in stack])
