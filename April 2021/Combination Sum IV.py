966cd90ac602eae9db943961b1e85cbcfb76da13
Parikshit Gupta <45290746+parikshitgupta1@users.noreply.github.com>
2021-04-19 21:02:27 +0530

    day 19

---
+++ April 2021/Combination Sum IV.py
@@ -0,0 +1,15 @@
+class Solution:
+   def combinationSum4(self, nums, target):
+        
+        dp = [0] * (target+1)
+        dp[0] = 1
+        nums.sort()
+
+        for i in range(1, target+1):
+            for j in range(len(nums)):
+                if nums[j] <= i:
+                    dp[i] += dp[i - nums[j]]
+                else:
+                    break
+
+        return dp[target]
