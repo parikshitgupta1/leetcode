83e82c0609d666ddf4290c8a4f803ee08787ffd1
Parikshit Gupta <45290746+parikshitgupta1@users.noreply.github.com>
2021-04-25 19:57:26 +0530

    day 25

---
+++ April 2021/Rotate Image.py
@@ -0,0 +1,16 @@
+class Solution:
+    def rotate(self, matrix) -> None:
+        result = []
+        for i in range(0,len(matrix)):
+            store = []
+            for j in range(len(matrix)-1,-1,-1):
+                store.append(matrix[j][i])
+    
+            result.append(store)
+        for i in range(0,len(result)):
+            matrix[i] = result[i]
+        return matrix
+
+
+a = Solution()
+print(a.rotate([[1,2],[3,4]]))
