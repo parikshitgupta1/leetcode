f3d75cdccd736ece8ce4c68915ec6132313fdca2
Parikshit Gupta <45290746+parikshitgupta1@users.noreply.github.com>
2021-04-30 22:04:59 +0530

    day 29

---
+++ April 2021/Find First and Last Position of Element in Sorted Array.py
@@ -0,0 +1,15 @@
+class Solution:
+    def searchRange(self, nums, target):
+        if target not in nums:
+            return [-1, -1]
+        i = nums.index(target)
+        j = i
+        for x in range(i+1, len(nums)):
+            if nums[x]!=target:
+                break
+            j = x
+        return [i, j]
+
+
+a = Solution()
+print(a.searchRange([5,7,7,8,8,10], 8))
