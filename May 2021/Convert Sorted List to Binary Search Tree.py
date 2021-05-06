6c45f421863315d271b8d30d8b18bdcf469dff6b
Parikshit Gupta <45290746+parikshitgupta1@users.noreply.github.com>
2021-05-06 21:04:56 +0530

    day 6

---
+++ May 2021/Convert Sorted List to Binary Search Tree.py
@@ -0,0 +1,17 @@
+class Solution:
+    def sortedListToBST(self, head):
+        if not head:
+            return 
+        if not head.next:
+            return TreeNode(head.val)
+
+        slow, fast = head, head.next.next
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+        tmp = slow.next
+        slow.next = None
+        root = TreeNode(tmp.val)
+        root.left = self.sortedListToBST(head)
+        root.right = self.sortedListToBST(tmp.next)
+        return root
