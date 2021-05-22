32a203eaedca92a950a4bc974de1d67590d400c1
Parikshit Gupta <45290746+parikshitgupta1@users.noreply.github.com>
2021-05-22 23:32:56 +0530

    day 19

---
+++ May 2021/Minimum Moves to Equal Array Elements II.py
@@ -0,0 +1,8 @@
+class Solution:
+    def minMoves2(self, nums):
+        nums.sort()
+        mid=len(nums)//2
+        res=0
+        for n in nums:
+            res+=abs(n-nums[mid])
+        return res
