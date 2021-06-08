90b0eb3111e89864b9fb610f2dcb3d0c8c7aa5ee
Parikshit Gupta <45290746+parikshitgupta1@users.noreply.github.com>
2021-06-08 16:51:04 +0530

    day 8

---
+++ June 2021/Construct Binary Tree from Preorder and Inorder Traversal.py
@@ -0,0 +1,14 @@
+class Solution:
+    """
+    @param preorder : A list of integers that preorder traversal of a tree
+    @param inorder : A list of integers that inorder traversal of a tree
+    @return : Root of a tree
+    """
+    def buildTree(self, preorder, inorder):
+        # write your code here
+        if not inorder: return None # inorder is empty
+        root = TreeNode(preorder[0])
+        rootPos = inorder.index(preorder[0])
+        root.left = self.buildTree(preorder[1 : 1 + rootPos], inorder[ : rootPos])
+        root.right = self.buildTree(preorder[rootPos + 1 : ], inorder[rootPos + 1 : ])
+        return root
