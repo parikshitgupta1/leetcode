e80135f33d3254949c55b656ee185c552f267091
Parikshit Gupta <45290746+parikshitgupta1@users.noreply.github.com>
2021-07-27 20:49:28 +0530

    day 27

---
+++ July 2021/3Sum Closest.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def threeSumClosest(self, nums, target):
+        result, min_diff = 0, float("inf")
+        nums.sort()
+        for i in reversed(range(2, len(nums))):
+            if i+1 < len(nums) and nums[i] == nums[i+1]:
+                continue
+            left, right = 0, i-1
+            while left < right:
+                total = nums[left]+nums[right]+nums[i]
+                if total < target:
+                    left += 1
+                elif total > target:
+                    right -= 1
+                else:
+                    return target
+                if abs(total-target) < min_diff:
+                    min_diff = abs(total-target)
+                    result = total
+        return result
