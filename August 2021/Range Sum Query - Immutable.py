1eda110f4bdf66f0a87462f09c73848ad19fb781
Parikshit Gupta <45290746+parikshitgupta1@users.noreply.github.com>
2021-08-16 21:13:39 +0530

    day 16

---
+++ August 2021/Range Sum Query - Immutable.py
@@ -0,0 +1,18 @@
+class NumArray:
+
+    def __init__(self, nums: List[int]):
+        if len(nums) == 0:
+            self._sums = [0]
+            return
+        elif len(nums) == 1:
+            self._sums = [nums[0]]
+        else:
+            self._sums = [nums[0]]
+            for i in range(1, len(nums)):
+                self._sums.append(self._sums[i - 1] + nums[i])
+
+    def sumRange(self, i: int, j: int) -> int:
+        if i == 0:
+            return self._sums[j]
+        
+        return self._sums[j] - self._sums[i - 1]
