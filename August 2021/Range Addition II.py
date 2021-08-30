d527c4fbf024d5c1a1c8b172e4a95789581e4361
Parikshit Gupta <45290746+parikshitgupta1@users.noreply.github.com>
2021-08-30 20:54:30 +0530

    day 30

---
+++ August 2021/Range Addition II.py
@@ -0,0 +1,8 @@
+from typing import List
+
+class Solution:
+    def maxCount(self, m: int, n: int, ops: List[List[int]]) -> int:
+        for x, y in ops:
+            m = min(m, x)
+            n = min(n, y)
+        return m * n 
