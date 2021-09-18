892cb5890b347577505001eacb8018f09cf2d69e
Parikshit Gupta <45290746+parikshitgupta1@users.noreply.github.com>
2021-09-18 21:21:05 +0530

    day 18

---
+++ September 2021/Expression Add Operators.py
@@ -0,0 +1,20 @@
+class Solution:
+    def addOperators(self, num, target):
+        def dfs(idx, tmp, tot, last, res):
+            if idx == len(num):
+                if tot == target:
+                    res.append(tmp)
+                return
+            for i in range(idx, len(num)):
+                x = int(num[idx: i + 1])
+                if idx == 0:
+                    dfs(i + 1, str(x), x, x, res)
+                else:
+                    dfs(i + 1, tmp + "+" + str(x), tot + x, x, res)
+                    dfs(i + 1, tmp + "-" + str(x), tot - x, -x, res)
+                    dfs(i + 1, tmp + "*" + str(x), tot - last + last * x, last * x, res)
+                if x == 0:
+                    break
+        res = []
+        dfs(0, "", 0, 0, res)
+        return res
