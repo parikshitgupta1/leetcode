7d96e74c7ad7a40b8975d9d1826c17f42f27cd38
Parikshit Gupta <45290746+parikshitgupta1@users.noreply.github.com>
2021-09-30 22:25:38 +0530

    day 30

---
+++ September 2021/Sort Array By Parity II.py
@@ -0,0 +1,15 @@
+from typing import List
+
+
+class Solution:
+    def sortArrayByParityII(self, nums: List[int]) -> List[int]:
+        
+        i, j, n = 0, 1, len(nums)
+        while j < n and i < n:
+            if nums[i] % 2 == 0:
+                i += 2
+            elif nums[j] % 2 == 1:
+                j += 2
+            else:
+                nums[i], nums[j] = nums[j], nums[i]
+        return nums
