660b26b52acaeafc959746e8f06cf65138024b4c
Parikshit Gupta <45290746+parikshitgupta1@users.noreply.github.com>
2021-10-02 20:27:22 +0530

    day 2

---
+++ October 2021/Dungeon Game.py
@@ -0,0 +1,14 @@
+class Solution:
+    def calculateMinimumHP(self, dungeon: List[List[int]]) -> int:
+        rows, cols = len(dungeon), len(dungeon[0])
+        dp = [[math.inf for x in range(cols+1)] for y in range(rows+1)]
+        dp[rows][cols-1], dp[rows-1][cols] = 1, 1
+        
+        for i in range(rows-1, -1, -1):
+            for j in range(cols-1, -1, -1):
+                dp[i][j] = min(dp[i][j+1], dp[i+1][j]) - dungeon[i][j]
+                
+                if dp[i][j]<1:
+                    dp[i][j] = 1
+                    
+        return dp[0][0]
