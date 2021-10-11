c8515fd25c15cb9fa90023a120f8947fd2979f50
Parikshit Gupta <45290746+parikshitgupta1@users.noreply.github.com>
2021-10-11 22:01:28 +0530

    day 11

---
+++ October 2021/Diameter of Binary Tree.py
@@ -0,0 +1,12 @@
+class Solution:
+    def diameterOfBinaryTree(self, root: TreeNode) -> int:
+        def _diameterOfBinaryTree(root: TreeNode) -> int:
+            if root is None:
+                return 0, 0
+
+            l, lbelow = _diameterOfBinaryTree(root.left)
+            r, rbelow = _diameterOfBinaryTree(root.right)
+
+            return max(l + 1, r + 1) , max(l + 1 + r, lbelow, rbelow)
+
+        return max(max(_diameterOfBinaryTree(root)) - 1, 0)
