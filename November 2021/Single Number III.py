376825ebd33d6de8c692b5daf7671dc0f70c2cf2
Parikshit Gupta <45290746+parikshitgupta1@users.noreply.github.com>
2021-11-06 22:08:29 +0530

    day 6

---
+++ November 2021/Single Number III.py
@@ -0,0 +1,9 @@
+from typing import List
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        ans = []
+        for i in nums:
+            if nums.count(i)==1:
+                ans.append(i)
+        return ans
