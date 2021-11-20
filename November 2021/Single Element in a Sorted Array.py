9865bfdf5f21ea9d1bfab9485bb850d0d053380b
Parikshit Gupta <45290746+parikshitgupta1@users.noreply.github.com>
2021-11-20 11:48:36 +0530

    day 20

---
+++ November 2021/Single Element in a Sorted Array.py
@@ -0,0 +1,7 @@
+from collections import Counter
+class Solution:
+    def singleNonDuplicate(self, nums: List[int]) -> int:
+        x=Counter(nums)
+        for i in range(len(nums)):
+            if x[nums[i]]==1:
+                return nums[i]
