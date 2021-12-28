eba6c11740dc7597204af127c0f4c2163376294f
Parikshit Gupta <45290746+parikshitgupta1@users.noreply.github.com>
2021-12-28 21:43:35 +0530

    day 28

---
+++ December 2021/Middle of the Linked List.py
@@ -0,0 +1,27 @@
+
+# Definition for singly-linked list.
+class ListNode:
+     def __init__(self, x):
+         self.val = x
+         self.next = None
+
+def getCount(temp): 
+        count = 0 
+  
+        while (temp): 
+            count += 1
+            temp = temp.next
+        return count 
+
+class Solution:
+    def middleNode(self, head: ListNode) -> ListNode:
+        l = getCount(head)
+        mid = 0 + (l-0)/2
+        if l % 2 != 0:
+            for i in range(int(mid)):
+                head = head.next
+            return head
+        else:
+            for i in range(int(mid)):
+                head = head.next
+            return head
