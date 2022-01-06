63554779a02aa73272ccc7ef9026126dd8cf818e
Parikshit Gupta <45290746+parikshitgupta1@users.noreply.github.com>
2022-01-06 22:03:57 +0530

    day 6

---
+++ January 2022/Car Pooling.py
@@ -0,0 +1,17 @@
+from typing import List
+
+class Solution:
+	def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
+		lst = []
+
+		for n, start, end in trips:
+			lst.append((start, n))
+			lst.append((end, -n))
+		lst.sort()
+
+		passengers = 0
+		for loc in lst:
+			passengers += loc[1]
+			if passengers > capacity:
+				return False
+		return True
